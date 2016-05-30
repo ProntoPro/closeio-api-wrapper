82fdfcd40887aac81603efecb299035745fa349a
Giacomo Furlan <giacomo.furlan@prontopro.it>
2016-05-30 19:11:15 +0200

    Fix JsonSerializable for 0 values

--- src/LooplineSystems/CloseIoApiWrapper/Library/JsonSerializableHelperTrait.php
+++ src/LooplineSystems/CloseIoApiWrapper/Library/JsonSerializableHelperTrait.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
-* Close.io Api Wrapper - LLS Internet GmbH - Loopline Systems
-*
-* @link      https://github.com/loopline-systems/closeio-api-wrapper for the canonical source repository
-* @copyright Copyright (c) 2014 LLS Internet GmbH - Loopline Systems (http://www.loopline-systems.com)
-* @license   https://github.com/loopline-systems/closeio-api-wrapper/blob/master/LICENSE (MIT Licence)
-*/
+ * Close.io Api Wrapper - LLS Internet GmbH - Loopline Systems
+ *
+ * @link      https://github.com/loopline-systems/closeio-api-wrapper for the canonical source repository
+ * @copyright Copyright (c) 2014 LLS Internet GmbH - Loopline Systems (http://www.loopline-systems.com)
+ * @license   https://github.com/loopline-systems/closeio-api-wrapper/blob/master/LICENSE (MIT Licence)
+ */
 
 namespace LooplineSystems\CloseIoApiWrapper\Library;
 
@@ -19,6 +19,8 @@ trait JsonSerializableHelperTrait
     {
         return array_map(function ($value) {
             return $value instanceof \DateTime ? $value->format('c') : $value;
-        }, array_filter(get_object_vars($this)));
+        }, array_filter(get_object_vars($this), function ($value) {
+            return $value !== null || $value !== '';
+        }));
     }
 }
