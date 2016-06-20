b2c48ffc7a9dca1a81de1c4dd6bc56675baa3f46
Giacomo Furlan <giacomo.furlan@prontopro.it>
2016-06-20 16:28:17 +0200

    Added ActivityApi::getAllCallsBetween

--- src/LooplineSystems/CloseIoApiWrapper/Api/ActivityApi.php
+++ src/LooplineSystems/CloseIoApiWrapper/Api/ActivityApi.php
@@ -30,6 +30,52 @@ class ActivityApi extends AbstractApi
     }
 
     /**
+     * Find all the calls between 2 dates. If none is specified, get all calls.
+     * @param \DateTime|null $from
+     * @param \DateTime|null $to
+     * @param string|null $leadId
+     */
+    public function getAllCallsBetween(\DateTime $from = null, \DateTime $to = null, $leadId)
+    {
+        $query = [];
+        if ($from) {
+            $query['date_created__gt'] = $from->format(DATE_ISO8601);
+        }
+
+        if ($to) {
+            $query['date_created__lt'] = $to->format(DATE_ISO8601);
+        }
+
+        if ($leadId) {
+            $query['leadId'] = $leadId;
+        }
+
+        $limit = 100;
+        $skip = 0;
+        do {
+            $continue = false;
+            $count = 0;
+            $query = array_merge($query, ['_limit' => $limit, '_skip' => $skip]);
+
+            $apiRequest = $this->prepareRequest('get-calls', null, [], $query);
+
+            /** @var CloseIoResponse $result */
+            $result = $this->triggerGet($apiRequest);
+
+            if ($result->getReturnCode() === 200) {
+                foreach ($result->getData()[CloseIoResponse::GET_ALL_RESPONSE_DATA_KEY] as $data) {
+                    $count++;
+                    yield new Activity($data);
+                }
+
+                $continue = $result->getData()[CloseIoResponse::GET_ALL_RESPONSE_HAS_MORE_KEY];
+            }
+
+            $skip += $limit;
+        } while ($continue && $count === $limit);
+    }
+
+    /**
      * @param null $limit
      * @param null $skip
      *
